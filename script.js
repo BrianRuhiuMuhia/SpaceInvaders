3ca5f28ef9e7670eaa6b1788b2ef4a8c4e204cd8
brianruhiu <brianmuhia220@gmail.com>
2022-11-02 14:23:22 +0300

    second-commit

--- script.js
+++ script.js
@@ -1,174 +1,261 @@
-const canvas=document.querySelector("canvas")
+ 2const canvas=document.querySelector("canvas")
 const ctx=canvas.getContext("2d")
-const canvas_width=canvas.width=600
+const canvas_width=canvas.width=800
 const canvas_height=canvas.height=600
 window.addEventListener("load",function()
 {
-    let bulletArray=[]
-    const Game={
-    draw:function()
+    const max=100
+    let count=0
+    let bullets=[]
+    let enemyBullets=[]
+    let gridArray=[]
+    function Game()
     {
-//must be inherited
-   this.drawImage()
-    },
-    drawImage:function()
-    {
-ctx.drawImage(this.sprite,this.x,this.y,this.width,this.height)
-    },
-    update:function()
-    {
-//must be overidden
+        this.background=new Background(this)
+        this.player=new Player(this)
+        this.grid=new Grid(this)
     }
-}
-class Grid{
-    constructor()
+    Game.prototype.draw=function(asset)
     {
+        ctx.drawImage(asset.sprite,asset.x,asset.y,asset.width,asset.height)
+    }
+    Game.prototype.update=function()
+    {
+        this.player.update()
+        bullets=bullets.filter((bullet)=>!bullet.delete)
+        bullets.forEach((bullet)=>{
+            bullet.update("player")
+        })
+        this.grid.update()
+    }
+    Game.prototype.drawImages=function(deltaTime)
+    {
+        this.background.draw()
+        this.player.draw()
+        bullets.forEach((bullet)=>{
+            bullet.draw()
+        })
+        this.grid.enemyArray.forEach((enemy)=>{
+            enemy.draw()   
+            enemy.update()
+        })
+       
+        if(count>max)
+        {
+            gridArray.push(new Grid(this))
+            this.grid.enemyArray[Math.floor(Math.random() * this.grid.enemyArray.length)].shoot()
+            count=0
+        }
+else{
+    count+=deltaTime
+}
+        enemyBullets=enemyBullets.filter((bullet)=>!bullet.delete)
+        enemyBullets.forEach((bullet)=>{
+            
+            bullet.draw("enemy")
+            bullet.update('enemy')
+        })
+    }
+    function Background(game)
+    {
+        this.game=game
         this.x=0
         this.y=0
-        this.velocity={vx:0.1,vy:1}
-        this.enemyList=[]
-        const rows=6
-        const cols=6
-        for(let i=0;i<rows;i++)
+        this.width=canvas_width
+        this.height=canvas_height
+        this.sprite=document.getElementById("background")
+    }
+    Background.prototype.draw=function()
+    {
+        this.game.draw({
+            sprite:this.sprite,
+            x:this.x,
+            y:this.y,
+            width:this.width,
+            height:this.height
+        })
+    }
+    function Player(game)
+    {
+        this.game=game
+        this.width=50
+        this.height=50
+        this.x=canvas.width/2 -this.width
+        this.y=canvas_height-(60)
+        this.speed=20
+        this.sprite=document.getElementById("player")
+        this.bullet=undefined
+    }
+    Player.prototype.draw=function()
+    {
+        this.game.draw({
+            sprite:this.sprite,
+            x:this.x,
+            y:this.y,
+            width:this.width,
+            height:this.height
+        })
+    }
+    Player.prototype.moveLeft=function()
+    {
+        this.x-=this.speed
+    }
+    Player.prototype.moveRight=function()
+    {
+        this.x+=this.speed
+    }
+    Player.prototype.update=function()
+    {
+        if(this.x + this.width>canvas_width)
         {
-            for(let j=0;j<cols;j++)
-            {
-           this.enemyList.push(new Enemy({x:j,y:i},this))
-            }
+            this.x=canvas_width-this.width
         }
-        this.width=40 * cols +200
-       
-    }
-    update()
-    {
-        if(this.x + this.width>=canvas.width|| this.x<0)
-        { 
-            this.velocity.vx=-this.velocity.vx
-            this.y+=this.velocity.vy
+        if(this.x <0)
+        {
+            this.x=0
         }
-        this.x+=this.velocity.vx
-        console.log(this.x)
-        console.log(this.x + this.width)
     }
-}
-class Enemy{
-    constructor(position,grid)
+    Player.prototype.shoot=function()
     {
-this.grid=grid
-this.width=40
-this.height=40
-this.position=position
-this.x=this.grid.x + this.position.x 
-this.y=this.grid.y + this.position.y
-this.sprite=document.getElementById("enemy")
+this.bullet=new Bullet(this)
+bullets.push(this.bullet)
     }
-    draw()
+    function Bullet(ship)
     {
-        ctx.drawImage(this.sprite,this.x*this.width,this.y*this.height,this.width,this.height)
-    }
-    update()
-    {
-        this.draw()
-        this.x=this.grid.x + this.position.x
-        this.y=this.grid.y + this.position.y
-    }
+this.ship=ship
+this.x=this.ship.x + (this.ship.width/2-10)
+this.y=this.ship.y-20
+this.enemyY=this.ship.y+20
+this.speed=10
 
-}
-const background={
-    x:0,
-    y:0,
-    width:canvas_width,
-    height:canvas_height,
-    sprite:document.getElementById("background")
-}
-const player={
-    width:50,
-    height:50,
-    y:canvas_height-(80),
-    x:canvas_width/2-50,
-    speed:10,
-    sprite:this.document.getElementById("player"),
-    moveLeft:function()
-    {
-this.x-=this.speed
-    },
-    moveRight:function()
-    {
-this.x+=this.speed
-    },
-    update:function()
-    {
-        if(this.x + this.width>=canvas_width)
-        {
-        this.x=canvas_width-this.width
-        }
-        if(this.x <= 0)
-        {
-        this.x=0
-        }
-    },
-    shoot:function()
-    {
-        const bullet={
-            x:player.x + player.width/2-10,
-            y:player.y -30,
-            width:20,
-            height:20,
-            delete:false,
-            speed:5,
-            sprite:document.getElementById("bullet"),
-            update:function()
-            {
-                this.y-=this.speed
-                if(this.y<=0)
-                {
-this.delete=true
-bulletArray=bulletArray.filter((bullet)=>
+    }
+Bullet.prototype.draw=function(type)
 {
-    return !bullet.delete 
-})
-}
-}
-}
-Object.setPrototypeOf(bullet,player)
-bulletArray.push(bullet)
-gameArray.push(...bulletArray)
+    if(type==="enemy")
+    {
+        this.width=20
+this.height=20
+        this.sprite=document.getElementById("enemyBullet")
+        ctx.drawImage(this.sprite,this.x,this.enemyY,this.width,this.height)
+    }
+    else{
+        this.width=25
+this.height=25
+        this.sprite=document.getElementById("bullet")
+          this.ship.game.draw({
+        sprite:this.sprite,
+            x:this.x,
+            y:this.y,
+            width:this.width,
+            height:this.height,
+            delete:false
+    })
     }
 }
-document.addEventListener("keydown",function(e)
+Bullet.prototype.update=function(type)
 {
-switch(e.key)
+    //that's why i love java method overloading ,used if statement to try to minic method overloading
+if(type==="enemy")
 {
+    this.enemyY+=this.speed
+    if(this.enemyY>=canvas_width)
+    {
+        this.delete=true
+    }
+}
+else if(type==="player")
+{
+    this.y-=this.speed
+    if(this.y<=0)
+    {
+        this.delete=true
+    }
+}
+}
+function Grid(game)
+{
+    this.game=game
+this.x=0
+this.y=0
+this.cols=Math.floor(Math.random() * 6 +1)
+this.rows=Math.floor(Math.random()*6 +1)
+this.enemyArray=[]
+this.velocity={vx:0.1,vy:2}
+for(let i=0;i<this.rows;i++)
+{
+    for(let j=0;j<this.cols;j++)
+    {
+this.enemyArray.push(new Enemy(this,{x:j,y:i}))
+    }
+}
+this.width=30 * this.cols
+}
+Grid.prototype.update=function()
+{
+    if((this.x*30) + this.width>=canvas_width || this.x <0)
+    {
+        this.velocity.vx=-this.velocity.vx
+        this.y+=this.velocity.vy
+    }
+    this.x+=this.velocity.vx
+}
+function Enemy(grid,position)
+{
+this.grid=grid
+this.game=this.grid.game
+this.width=30
+this.height=30
+this.position=position
+this.x=this.grid.x + position.x
+this.y=this.grid.y + position.y
+this.sprite=document.getElementById("enemy")
+}
+Enemy.prototype.draw=function()
+{
+    ctx.drawImage(this.sprite,this.x*this.width,this.y*this.height,this.width,this.height)
+}
+Enemy.prototype.update=function()
+{
+    this.x=this.grid.x + this.position.x
+    this.y=this.grid.y + this.position.y
+}
+Enemy.prototype.shoot=function()
+{
+    enemyBullets.push(new Bullet({
+        sprite:this.sprite,
+        x:this.x * this.width,
+        y:this.y * this.height,
+        width:this.width,
+        height:this.height,
+        delete:false
+    }))
+}
+const game=new Game()
+    document.addEventListener("keydown",function(event)
+    {
+        switch(event.key)
+        {
 case "ArrowLeft":
-    player.moveLeft()
+    game.player.moveLeft()
     break
 case "ArrowRight":
-    player.moveRight()
-    break;
+    game.player.moveRight()
+    break
 case "ArrowUp":
-    player.shoot()
-    break;
-}
+    game.player.shoot()
+break
+        }
+    })
+    let interval=0
+    function animate(timeStamp)
+    {
+        let deltaTime=timeStamp-interval
+        ctx.clearRect(0,0,canvas_width,canvas_height)
+game.drawImages(deltaTime)      
+game.update()
+interval=timeStamp
+        requestAnimationFrame(animate)
+    }
+    animate(0)
 })
-const gameArray=[background,player]
-const grid=new Grid()
-gameArray.forEach((obj)=>
-{
-Object.setPrototypeOf(obj,Game)
-})
-function animate()
-{
-ctx.clearRect(0,0,canvas_width,canvas_height)
-gameArray.forEach((obj)=>{
-        obj.draw()
-        obj.update()
-})
-grid.update()
-grid.enemyList.forEach((enemy)=>
-{
-    enemy.update()
-})
-    requestAnimationFrame(animate)
-}
-animate()
-})
+
